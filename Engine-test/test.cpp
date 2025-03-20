16d85ff21f4d003f42ba0c11fd26ce46046c80f8
vshti <vvshtin@edu.hse.ru>
2025-03-20 13:22:56 +0300

    added few tests for vectors

--- Engine-test/test.cpp
+++ Engine-test/test.cpp
@@ -1,6 +1,73 @@
 #include "pch.h"
+#include "Vector.h"
+TEST(VectorTest, ZeroConstructor)
+{
+	Engine::Vector2D<float> vec;
+	EXPECT_FLOAT_EQ(vec.x, 0);
+	EXPECT_FLOAT_EQ(vec.y, 0);
+}
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+TEST(VectorTest, Constructor)
+{
+	Engine::Vector2D<float> vec(5.f, -3.f);
+	EXPECT_FLOAT_EQ(vec.x, 5.f);
+	EXPECT_FLOAT_EQ(vec.y, -3.f);
+}
+
+TEST(VectorTest, Length)
+{
+	Engine::Vector2D<float> vec(-4.f, -3.f);
+	EXPECT_FLOAT_EQ(vec.GetLength(), 5.f);
+}
+
+TEST(VectorTest, Addition)
+{
+	Engine::Vector2D<float> vec1(5.f, -3.f);
+	Engine::Vector2D<float> vec2(-3.f, -6.f);
+	Engine::Vector2D<float> vec3 = vec1 + vec2;
+	EXPECT_FLOAT_EQ(vec3.x, 2.f);
+	EXPECT_FLOAT_EQ(vec3.y, -9.f);
+}
+
+TEST(VectorTest, Substract)
+{
+	Engine::Vector2D<float> vec1(5.f, -3.f);
+	Engine::Vector2D<float> vec2(-3.f, -6.f);
+	Engine::Vector2D<float> vec3 = vec1 - vec2;
+	EXPECT_FLOAT_EQ(vec3.x, 8.f);
+	EXPECT_FLOAT_EQ(vec3.y, 3.f);
+}
+
+TEST(VectorTest, Negative)
+{
+	Engine::Vector2D<float> vec1(5.f, -3.f);
+	Engine::Vector2D<float> vec2 = -vec1;
+	EXPECT_FLOAT_EQ(vec2.x, -5.f);
+	EXPECT_FLOAT_EQ(vec2.y, 3.f);
+}
+
+TEST(VectorTest, Dot)
+{
+	Engine::Vector2D<float> vec1(5.f, -3.f);
+	Engine::Vector2D<float> vec2(-3.f, -6.f);
+	float result = vec1.Dot(vec2);
+	EXPECT_FLOAT_EQ(result, 3.f);
+}
+
+TEST(VectorTest, ScalarMultVec)
+{
+	Engine::Vector2D<float> vec1(5.f, -3.f);
+	float scalar = 3;
+	Engine::Vector2D<float> vec2 = scalar * vec1;
+	EXPECT_FLOAT_EQ(vec2.x, 15.f);
+	EXPECT_FLOAT_EQ(vec2.y, -9.f);
+}
+
+TEST(VectorTest, VecMultScalar)
+{
+	Engine::Vector2D<float> vec1(5.f, -3.f);
+	float scalar = 3;
+	Engine::Vector2D<float> vec2 = vec1 * scalar;
+	EXPECT_FLOAT_EQ(vec2.x, 15.f);
+	EXPECT_FLOAT_EQ(vec2.y, -9.f);
 }
--- Engine/Vector.h
+++ Engine/Vector.h
@@ -1,12 +1,26 @@
 #pragma once
 
-namespace ArkanoidGame
+namespace Engine
 {
 	template<typename T>
 	struct Vector2D
 	{
+		Vector2D() : x(0), y(0)
+		{ }
+		Vector2D(T newX, T newY) : x(newX), y(newY)
+		{ }
 		T x = (T)0;
 		T y = (T)0;
+
+		float GetLength()
+		{
+			return sqrtf(x * x + y * y);
+		}
+
+		T Dot(Vector2D& otherVector)
+		{
+			return x * otherVector.x + y * otherVector.y;
+		}
 	};
 
 	using Vector2Df = Vector2D<float>;
@@ -26,16 +40,35 @@ namespace ArkanoidGame
 	}
 
 	template<typename T>
+	Vector2D<T> operator-(const Vector2D<T>& lhs)
+	{
+		return { -lhs.x, -lhs.y };
+	}
+
+	template<typename T>
+	Vector2D<float> operator*(const Vector2D<T>& lhs, const float rhs)
+	{
+		return { lhs.x * rhs, lhs.y * rhs };
+	}
+
+	template<typename T>
+	Vector2D<float> operator*(const float lhs, const Vector2D<T>& rhs)
+	{
+		return { lhs * rhs.x, lhs * rhs.y };
+	}
+
+	template<typename T>
 	bool operator==(const Vector2D<T>& lhs, const Vector2D<T>& rhs)
 	{
 		return lhs.x == rhs.x && lhs.y == rhs.y;
 	}
 
 	template<typename T>
-	float GetVectorLength(const Vector2D<T>& vector)
+	bool operator!=(const Vector2D<T>& lhs, const Vector2D<T>& rhs)
 	{
-		return sqrtf(vector.x * vector.x + vector.y * vector.y);
+		return lhs.x != rhs.x || lhs.y != rhs.y;
 	}
+
 
 	// This complex template allows us to convert any vector type to any other vector type (like our Vector2D to SFML's Vector and vice versa)
 	template<typename U, typename V>
