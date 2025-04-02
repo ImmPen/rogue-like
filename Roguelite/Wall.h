b06fbd0e643e6694719a547c5c824e77830c3187
ImmPen <53078244+ImmPen@users.noreply.github.com>
2025-04-02 21:59:50 +0300

    started making walls and floor

---
+++ Roguelite/Wall.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "GameWorld.h"
+#include "SpriteRendererComponent.h"
+#include "RenderSystem.h"
+#include "GameObject.h"
+#include "SpriteColliderComponent.h"
+#include "ResourceSystem.h"
+
+namespace Roguelite
+{
+	class Wall
+	{
+	public:
+		Wall(const Engine::Vector2Df position, int textureMapIndex);
+	private:
+		Engine::GameObject* gameObject;
+	};
+}
